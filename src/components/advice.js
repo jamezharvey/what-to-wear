a3b4f0b720b768dbfd4aca39b74753136f3173da
jamezharvey <jamesharvey1080@gmail.com>
2022-10-03 21:11:39 +0100

    test not working thus far

--- src/components/advice.js
+++ src/components/advice.js
@@ -17,11 +17,12 @@ const Advice = () => {
     setLoading(false)
   }, [city]);
 
-  
+    if(loading){
+    return <div></div>
+  }
+  if(weather){
     return (
-    <>
-      {loading ? null : weather ? (
-         <>
+      <>
       <div className="d-flex justify-content-center">
         <h1 className="weather_condition" id="hello">{weather_condition[weather.condition.text].concat(temperature_clothing[(Math.round(weather.temp_c).toString())])}</h1>
       </div>
@@ -29,18 +30,18 @@ const Advice = () => {
                 <a className="btn btn-dark btn-lg mt-2" href="/" role="button" id="button">Back</a>
             </div>
     </>
-      ) : (
-        <>
-        <div className="d-flex justify-content-center">
-        <h1 className="weather_condition" id="hello">Are you in a real city? Or did you just spell it wrong? Try again...</h1>
-      </div>
-      <div className="text-center">
-                <a className="btn btn-dark btn-lg mt-2" href="/" role="button" id="button">Back</a>
-            </div>
-      </>
-      )}
-    </>
-  );
-};
+    );
+  };
+  return (
+  <>
+    <div className="d-flex justify-content-center">
+      <h1 className="weather_condition" id="hello">Are you in a real city? Or did you just spell it wrong? Try again...</h1>
+    </div>
+    <div className="text-center">
+              <a className="btn btn-dark btn-lg mt-2" href="/" role="button" id="button">Back</a>
+          </div>
+  </>
+);
+}
 
 export default Advice;
