2a1aaba3c9141237b04a1e00f59bb9200674d8a6
jamezharvey <jamesharvey1080@gmail.com>
2022-10-03 21:58:04 +0100

    doesnt work but I learned things

--- src/components/advice.js
+++ src/components/advice.js
@@ -1,47 +1,73 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getWeather } from "../utils/api";
-import { weather_condition, temperature_clothing } from "../utils/weather-outcomes";
-import "../App.css"
+import {
+  weather_condition,
+  temperature_clothing,
+} from "../utils/weather-outcomes";
+import "../App.css";
 
 const Advice = () => {
   const [weather, setWeather] = useState("");
-  const[loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
   const { city } = useParams();
 
   useEffect(() => {
-
     getWeather(city).then((weatherFromApi) => {
+      console.log(city);
       setWeather(weatherFromApi);
+      setLoading(false);
     });
-    setLoading(false)
   }, [city]);
 
-    if(loading){
-    return <div></div>
-  }
-  if(weather){
-    return (
-      <>
-      <div className="d-flex justify-content-center">
-        <h1 className="weather_condition" id="hello">{weather_condition[weather.condition.text].concat(temperature_clothing[(Math.round(weather.temp_c).toString())])}</h1>
-      </div>
-      <div className="text-center">
-                <a className="btn btn-dark btn-lg mt-2" href="/" role="button" id="button">Back</a>
-            </div>
-    </>
-    );
-  };
-  return (
-  <>
-    <div className="d-flex justify-content-center">
-      <h1 className="weather_condition" id="hello">Are you in a real city? Or did you just spell it wrong? Try again...</h1>
-    </div>
-    <div className="text-center">
-              <a className="btn btn-dark btn-lg mt-2" href="/" role="button" id="button">Back</a>
+  if (!loading) {
+    if (weather) {
+      return (
+        <>
+          <div className="d-flex justify-content-center">
+            <h1 className="weather_condition" id="hello">
+              {weather_condition[weather.condition.text].concat(
+                temperature_clothing[Math.round(weather.temp_c).toString()]
+              )}
+            </h1>
           </div>
-  </>
-);
-}
+          <div className="text-center">
+            <a
+              className="btn btn-dark btn-lg mt-2"
+              href="/"
+              role="button"
+              id="button"
+            >
+              Back
+            </a>
+          </div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <div className="d-flex justify-content-center">
+            <h1 className="weather_condition" id="hello">
+              Are you in a real city? Or did you just spell it wrong? Try
+              again...
+            </h1>
+          </div>
+          <div className="text-center">
+            <a
+              className="btn btn-dark btn-lg mt-2"
+              href="/"
+              role="button"
+              id="button"
+            >
+              Back
+            </a>
+          </div>
+        </>
+      );
+    }
+  } else {
+    return <div>Loading...</div>;
+  }
+};
 
 export default Advice;
